018a80c0878a6dce0b65ca6addde5436b9acd5f5
AnastasiiaKrush <anastasiia.krushelnytska@gmail.com>
2020-04-09 19:02:58 +0300

    Fixed validation on user creation action

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,20 +2,20 @@
 
 @section('content')
    <div id="success-message" class="col valid-feedback"></div>
-   <form id="user-create-form" name="user" class="needs-validation" novalidate>
+   <form id="user-create-form" name="user" class="needs-validation" novalidate >
         <label>Create new user</label>
         <fieldset>
             <div class="row">
                 <div class="col">
-                    <input name="name"type="text" class="form-control" placeholder="Name" required >
+                    <input name="name" type="text" class="form-control" placeholder="Name" required >
                 </div>
                 <div class="col">
-                    <input id="email" name="email"type="email" class="form-control" placeholder="Email" required>
+                    <input id="email" name="email" type="email" class="form-control" placeholder="Email" required>
                 </div>
             </div>
             <div class="row">
                 <div class="col">
-                    <input id="birthday" name="birthday"type="date" min="1900-01-01" class="form-control" required>
+                    <input id="birthday" name="birthday" type="date" min="1900-01-01" class="form-control" required>
                 </div>
                 <div class="col">
                     <input id="phone-number" name="phone_number" type="number" class="form-control" placeholder="Phone number">
@@ -26,7 +26,7 @@
                     <input name="password" id="pass" type="password" class="form-control" placeholder="Password" required >
                 </div>
                 <div class="col">
-                    <input name="password_confirm" id="pass-conf" type="password" class="form-control" placeholder="Confirm password" required>
+                    <input name="password_confirm" id="pass-conf" type="password" class="form-control" placeholder="Confirm password">
                 </div>
             </div>
             <div id="error-message" class="col invalid-feedback"></div>
@@ -43,7 +43,8 @@
                    return local.toJSON().slice(0,10);
                });
 
-               $('#birthday').attr('max', new Date().toDateInputValue());
+                let currentDate = new Date().toDateInputValue();
+                $('#birthday').attr('max', currentDate);
 
                window.addEventListener('load', function() {
                    let forms = document.getElementsByClassName('needs-validation');
@@ -55,17 +56,37 @@
                                errorMessageElem = document.getElementById("error-message"),
                                successMessageElem = document.getElementById("success-message"),
                                phoneNumberElem = document.getElementById("phone-number"),
+                               birthdayElem = document.getElementById("birthday"),
+                               emailElem = document.getElementById("email"),
+
+                               birthdayCheck = (currentDate >= birthdayElem.value) ? true : false,
                                passMatch = (passwordElem.value == passwordConfirmElem.value) ? true : false,
                                formCheck = (form.checkValidity() !== false) ? true : false,
                                phoneNumberLength = (phoneNumberElem.value.length < 16) ? true : false;
+
                            e.preventDefault();
 
-                           if (!passMatch || !formCheck || !phoneNumberLength) {
-                               if(!passMatch) setMessage(errorMessageElem, "The password confirm and password must match.");
+                           if (passMatch) {
+                               passwordElem.classList.remove('is-invalid');
+                               passwordConfirmElem.classList.remove('is-invalid');
+                           }
+
+                           if (!passMatch || !formCheck || !phoneNumberLength || !birthdayCheck) {
+                               if(!passMatch) {
+                                   setMessage(errorMessageElem, "The password confirm and password must match.");
+                                   passwordElem.classList.add('is-invalid');
+                                   passwordConfirmElem.classList.add('is-invalid');
+                               }
+
                                if(!phoneNumberLength) setMessage(errorMessageElem, "Please, enter phone number a max of 15 digits.");
                                if(!formCheck) setMessage(errorMessageElem, "Please, fill all required fields.");
+                               if(!birthdayCheck && !formCheck) setMessage(errorMessageElem, "Please, enter correct birthday date.");
+
+                               emailElem.classList.remove('is-invalid');
                                form.classList.add('was-validated');
+
                                e.stopPropagation();
+
                                return;
                            }
 
@@ -77,13 +98,17 @@
                                async: true,
                                data: $(form).serialize(),
                            }).done(function() {
+                               emailElem.classList.remove('is-invalid');
                                setMessage(successMessageElem, "User created successfully.<br/>");
                                setTimeout(() => {setMessage(successMessageElem,'')}, 2000);
                                document.getElementById("user-create-form").reset();
                                form.classList.remove('was-validated');
                            }).fail(function (request) {
                                let errors = JSON.parse(request.responseText).errors;
-                               if (errors.email) setMessage(errorMessageElem, errors.email[0]);
+                               if (errors.email) {
+                                   emailElem.classList.add('is-invalid');
+                                   setMessage(errorMessageElem, errors.email[0]);
+                               }
                            });
                        }, false);
                    });
